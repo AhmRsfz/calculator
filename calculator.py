4c7915f9a8f9b89940925091682ae44b61004990
AhmdRsfz <chanelinfisibility@gmail.com>
2024-04-05 21:04:29 +0700

    Simple calculator code in python

---
+++ calculator.py
@@ -0,0 +1,62 @@
+# Auto clear for testing
+import os;os.system('clear')
+
+# Loop lanjut/tidak
+def loop():
+
+    while True:
+        x = input('lanjut? (y/n)')
+        if x in ['y','Y']:
+            break
+        elif x in ['n','N']:
+            exit()
+        else:
+            print('\n','x'*5,'undefined','x'*5,'\n')
+            continue
+
+# Loop input nomor
+def insrt(c):
+
+    while True:
+        try:
+            a = float(input('first: '))
+            print(c)
+            b = float(input('second: '))
+
+        except ValueError:
+            print('\n','x'*5,'undefined','x'*5,'\n')
+            continue
+            
+        if c == '+':
+            return print('result: ',a+b,'\n')
+        elif c == '-':
+            return print('result: ',a-b,'\n')
+        elif c == 'x':
+            return print('result: ',a*b,'\n')
+        elif c == '/':
+            return print('result: ',a/b,'\n')
+        else:
+            print('\n','x'*5,'undefined','x'*5,'\n')
+            break
+
+# Main loop
+def main():
+
+    while True:
+        print('\nCalculator\n ditambah > +\n dikurangi > -\n dikali > x\n dibagi > /\n','='*15)
+        inpt = input('>> ')
+        if inpt == '+':
+            insrt('+')
+        elif inpt == '-':
+            insrt('-')
+        elif inpt == 'x':
+            insrt('x')
+        elif inpt == '/':
+            insrt('/')
+        else:
+            print('\n','x'*5,'undefined','x'*5,'\n')
+            continue
+        loop()
+        
+# Run code
+main()
